633ae2258bcda5ad819a65acf428328de52af704
souravganguli <gangulisourav94@gmail.com>
2021-10-30 20:24:34 +0530

    add title

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const http = require('http');
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('start.html').pipe(res)
 });
 
 server.listen(port,() => {
